1458c4a3b15cdc2b5cdab7c09682aaadb08cb91b
Alvin Ng <ngalvin93@gmail.com>
2019-08-06 20:42:18 -0500

    completed renderRectangle.js

--- renderRectangle.js
+++ renderRectangle.js
@@ -2,20 +2,21 @@
 function renderRectangle(rectangle) {
     return `
         <div class="text-center mt-5">
-            <code>${JSON.stringify(rectangle)}</code>
+            <div style="
+            width: ${rectangle.width}px;
+            height: ${rectangle.height}px;
+            background-color: ${rectangle.color}">
+            </div> 
         </div>
     `
 }
 
 function rectangle() {
     var content = document.getElementById('content');
-
     var rectangleAbstraction = {
         width: 100,
         height: 200,
         color: "#FF00FF"
     }
-
     content.innerHTML = renderRectangle(rectangleAbstraction);
-
 }
